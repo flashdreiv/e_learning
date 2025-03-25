260e847bdbdc4091e59717f1c9b2b1c7d922380c
Dreivan Orprecio <drei@Dreivans-MacBook-Air.local>
2025-03-25 19:29:26 +0800

    Use backend api as env variable

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import useStore from "../store/useStore";
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 api.interceptors.request.use(
